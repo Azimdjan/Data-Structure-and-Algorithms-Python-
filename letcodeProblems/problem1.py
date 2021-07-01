3e9c2dd1be032c4e04f8d8712a8f7ef0c76902cd
Azimdjan <azimdjan18@gmail.com>
2021-07-01 18:49:13 +0500

    introduction to hash tables

---
+++ letcodeProblems/problem1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target: int):
+        myList = []
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if (target == nums[i] + nums[j]) and (i != j):
+                    myList.append(i)
+                    myList.append(j)
+        return myList
+
+if __name__ == '__main__':
+    myList = [1,3,23,54,234,543]
+    new = Solution()
+    print(new.twoSum(myList,24))
